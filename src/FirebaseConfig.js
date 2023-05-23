003b3bbaf663478715f249bcc4c9b54b5bdeb531
DonaldBowler44 <96385571+DonaldBowler44@users.noreply.github.com>
2023-05-23 09:33:13 -0500

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjpOrpUj8EXm0-Oy2QLgpAaBhowLJ083Y",
-  authDomain: "portfoliotwo-22873.firebaseapp.com",
-  projectId: "portfoliotwo-22873",
-  storageBucket: "portfoliotwo-22873.appspot.com",
-  messagingSenderId: "130440895762",
-  appId: "1:130440895762:web:14d693b00bb79cf86b27cb"
+  apiKey: "LOCATEDINPROJECT",
+  authDomain: "LOCATEDINPROJECT",
+  projectId: "LOCATEDINPROJECT",
+  storageBucket: "LOCATEDINPROJECT",
+  messagingSenderId: "LOCATEDINPROJECT",
+  appId: "LOCATEDINPROJECT"
 };
 
 
